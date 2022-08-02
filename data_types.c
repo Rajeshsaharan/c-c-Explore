7c47c701d17e1949060c02ecc460cdbc1c543873
rajeshsaharan <16072000rajesh@gmail.com>
2022-08-02 23:25:23 +0530

    c

---
+++ data_types.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+main(){
+//char is an data types that contains char information according to ascii table
+//char is lies between two ASTROPHER LIKE '' 'S' 'A' _ every character can be found on ascii table
+// multiple charcter in double quotes represent string
+// inegar number are real number without any decimal point like -1, 20, 30 ,40, -39
+//real number cant start from 0(unless the number is zero) if you type real number(integar) start with zero c compiler will understand it was octal or hexadecimal number
+//floating numer are decimal point number like 0.34, 3.14 etc
+//floating number can start from zero without any problem
+//floating number takes more memory than real number
+// (/n) are used for new line
+
+
+printf("hello world");
+//using char
+printf("hey my name is rajesh it start from %C\n", 'R');
+//using integar
+printf("hey i m brt %d student \n", 3);
+//using floating number
+printf("hey i scored %f parecent in 12th\n", 86.20);
+return 0;
+
+}
