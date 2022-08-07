82e94a156dd3b6324a8d4c172fb2f869d3de8104
rajeshsaharan <16072000rajesh@gmail.com>
2022-08-07 17:32:21 +0530

    c

---
+++ Header files & define Constant/define.h
@@ -0,0 +1,7 @@
+// define is used to create a constant across the project
+
+#define AGE 21
+#define NAME "rajesh"
+
+//convention---> use define name always in caps lock so we can use them as understanding of constant
+
