4173a89d1f913cb881a0c2eff166e5c225d6d894
rajeshsaharan <16072000rajesh@gmail.com>
2022-08-07 17:18:59 +0530

    c

--- variableName.c
+++ variableName.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 main(){
+//always define variable before you use them
+
 //variable name start from albhabate & cant use underscore
 // to define charchater in c language ---> we can use char keyword
+//= is known as assinment operator variable = data
 char my_first_letter_of_name = 'r' ;// define char
 int my_number = 2020; // define integar
+//number always define without comma even there are a long number like 1000000
 float my_percentage_of_12 = 86.20; //define floating number
-//further cab be used in printf function
+//further can be used in printf function
 printf("%c, %d, %f\n", my_first_letter_of_name, my_number, my_percentage_of_12 );
 printf("hello world");
 return 0;
