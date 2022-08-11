f61c791cab8faf8f6b583a70f8a0df795642171c
rajeshsaharan <16072000rajesh@gmail.com>
2022-08-11 09:35:41 +0530

    c

---
+++ loop/caseSwitch.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    // case switch are used to replace with nested if else statments
+    // a simple program with case switch
+    int choice;
+    printf("please enter your choice");
+    scanf(" %d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+        printf("you seletced 1 ");
+        break;
+    case 2:
+        printf("you selected 2");
+        break;
+    case 3:
+        printf("you selected 3");
+        break; // break keyword is very important for stop the loop 
+
+    default:
+        printf("you selcted nothing");
+        break;
+    }
+
+    return 0;
+}
